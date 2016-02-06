674a2915143cdbbc0821f0995dc6478ee3d2f292
Dongseop Kwon <dongseop@gmail.com>
2016-02-06 12:57:37 +0900

    rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,16 @@ lock '3.3.5'
 set :application, 'bioc-viewer'
 set :repo_url, 'git@bitbucket.org:dongseop/bioc-viewer.git'
 
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.3.0'
+
+# in case you want to set ruby version from the file:
+# set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
